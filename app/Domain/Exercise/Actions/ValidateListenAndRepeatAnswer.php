8063366217a7afe8dda4f7f5cd995b9f275c4659
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-08-06 02:44:03 +0300

    standardize character comparison

--- app/Domain/Exercise/Actions/ValidateListenAndRepeatAnswer.php
+++ app/Domain/Exercise/Actions/ValidateListenAndRepeatAnswer.php
@@ -4,6 +4,7 @@ namespace App\Domain\Exercise\Actions;
 
 use App\Domain\Exercise\Interfaces\Validator;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class ValidateListenAndRepeatAnswer extends ValidateAnswer implements Validator
 {
@@ -11,6 +12,11 @@ class ValidateListenAndRepeatAnswer extends ValidateAnswer implements Validator
     {
         $res = Http::attach('file', file_get_contents($value), 'test.wav')->post(env('PYTHON_HOST') . '/detect-voice');
 
-        return $this->extra_data['letter'] === $res->body();
+        return $this->standardizeCharacter($this->extra_data['letter']) === $this->standardizeCharacter($res->body());
+    }
+
+    protected function standardizeCharacter(string $char): string
+    {
+        return Str::replace(['1', 'َ', 'ُ', 'ِ'], ['', 'ا', 'و', 'ي'], $char);
     }
 }
